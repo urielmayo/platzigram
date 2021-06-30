2bf95de435b906d2afccafc506ad9009abd5771a
urielmayo <urimayo00@gmail.com>
2021-06-29 23:05:59 -0300

    addign source data from platzi course

--- platzigram/posts/views.py
+++ platzigram/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from datetime import datetime
 fragrances = [
     {
         'brand':'Versace',
@@ -22,6 +22,36 @@ fragrances = [
     },
 ]
 
+posts = [
+    {
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/600?image=1036',
+    },
+    {
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/800/?image=903',
+    },
+    {
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/500/700/?image=1076',
+    }
+]
+
 # Create your views here.
 def list_posts(request):
     """list existing posts"""
@@ -41,7 +71,7 @@ def list_posts(request):
 #funcion de ejemplo que retorna un template
 def run_template(request):
 
-    context ={
-        'fragrances': fragrances,
+    contexto ={
+        'posts': posts,
     }
-    return render(request, 'feed.html',{'fragrances': fragrances})
+    return render(request, 'feed.html',context=contexto)
