0abf2afb621a8ae1357559bb7bb4542c077ae153
urielmayo <urimayo00@gmail.com>
2021-07-02 00:43:10 -0300

    adding signup option

--- platzigram/users/views.py
+++ platzigram/users/views.py
@@ -2,6 +2,10 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
+from django.contrib.auth.models import User
+
+from users.models import Profile
+
 # Create your views here.
 def login_view(request):
     
@@ -29,4 +33,32 @@ def logout_view(request):
     return redirect('login')
 
 def signup_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password_conf = request.POST['password_conf']
+
+        #validates password
+        if password != password_conf:
+            return render(request, 'users/signup.html',{'error':'Password confirmation differs from password'})
+        #verifies new username is unique
+        elif User.objects.filter(username=username):
+            return render(request, 'users/signup.html',{'error':'User already exists'})
+        
+        #register user
+        user = User.objects.create_user(
+            username=username,
+            password=password, 
+            first_name=request.POST['first_name'],
+            last_name=request.POST['last_name'],
+            email=request.POST['email']
+        )
+
+        profile = Profile(
+            user=user
+        )
+        profile.save()
+        return redirect('login')
+
+
     return render(request, 'users/signup.html')
