6992e1fe58b059ebdcbc1c5fbc4d32e462ac1372
urielmayo <urimayo00@gmail.com>
2021-07-05 02:10:47 -0300

    deleting useless functions

--- platzigram/posts/views.py
+++ platzigram/posts/views.py
@@ -3,27 +3,6 @@ from django.http import HttpResponse
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
 
-fragrances = [
-    {
-        'brand':'Versace',
-        'name': 'Eros',
-        'type': 'Eau de Toilette',
-        'picture': 'https://fimgs.net/mdimg/perfume/375x500.16657.jpg',
-    },
-    {
-        'brand':'Dior',
-        'name': 'Sauvage',
-        'type': 'Eau de Parfum',
-        'picture': 'https://perfumeriaspigmento.com.ar/media/catalog/product/cache/image/620x678/e9c3970ab036de70892d86c6d221abfe/3/3/3348901368247_100ml_1.jpg',
-    },
-    {
-        'brand':'Armani',
-        'name': 'Armani Code Profumo',
-        'type': 'Eau de Parfum',
-        'picture': 'https://fimgs.net/mdimg/perfume/375x500.34761.jpg',
-    },
-]
-
 posts = [
     {
         'title': 'Mont Blanc',
@@ -53,22 +32,6 @@ posts = [
         'photo': 'https://picsum.photos/500/700/?image=1076',
     }
 ]
-
-# Create your views here.
-def list_posts(request):
-    """list existing posts"""
-    #example
-
-    content = []
-    
-    for fragrance in fragrances:
-        content.append(f"""
-            <p><strong> {fragrance['brand']} </strong></p>
-            <p><small> {fragrance['name']} - {fragrance['type']}</small></p>
-            <figure><img src="{fragrance['picture']}"/></figure>
-        """)
-    
-    return HttpResponse(content)
 
 #funcion de ejemplo que retorna un template
 @login_required
